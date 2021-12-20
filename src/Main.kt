12b455e3e4b24eb8731a3c3ff8ac8da943067ae8
brianA1998 <brian.alaniz@mercadolibre.com>
2021-12-20 17:14:21 -0300

    -Add function addVehicle in Parking -Add function checkIn in Parking -Changing constructor parameters from ParkingSpace to Vehicle

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 
 
-import org.graalvm.compiler.debug.TTY.println
+
 import java.util.*
 
 fun main(){
@@ -8,8 +8,19 @@ fun main(){
     val moto = Vehicle("B222BBB",VehicleType.MOTORCYCLE, Calendar.getInstance())
     val minibus = Vehicle("CC333CC",VehicleType.MINIBUS,Calendar.getInstance())
     val bus = Vehicle("DD444DD",VehicleType.BUS, Calendar.getInstance(),"DISCOUNT_CARD_002")
+    val mutableSetA = mutableSetOf<Vehicle>( car, moto , minibus , bus);
     val parking = Parking(mutableSetOf(car,moto,minibus,bus))
-    println(parking.vehicles.add(car))
+    println(parking.vehicles.contains(car))
+    println(parking.vehicles.contains(moto))
+    println(parking.vehicles.contains(minibus))
+    println(parking.vehicles.contains(bus))
+    parking.addVehicle(bus)
+
+
+
+    val car2 = Vehicle("AA111AA",VehicleType.CAR, Calendar.getInstance(),"DISCOUNT_CARD_001")
+    val isCar2Inserted = parking.vehicles.add(car2)
+    println(isCar2Inserted)
 
 
 }
--- src/Parking.kt
+++ src/Parking.kt
@@ -1,1 +1,22 @@
-data class Parking(val vehicles : MutableSet<Vehicle>)
+data class Parking(val vehicles : MutableSet<Vehicle>){
+
+    //Verified capabilities the Parking
+    fun addVehicle(vehicle : Vehicle) {
+        if(vehicles.size <= 20){
+            checkIn(true)
+        }else{
+            checkIn(false)
+        }
+    }
+
+        //Verified the check-in
+        private fun checkIn(result : Boolean){
+            if(result){
+                println("Welcome to AlkeParking!")
+            }else{
+                println("Sorry, the check-in failed")
+            }
+        }
+
+
+}
