fbe53204769ef6b9cbb7a3ca88a6b88826e18a9f
brianA1998 <brian.alaniz@mercadolibre.com>
2021-12-20 14:06:36 -0300

    Se agregaron las dataclass de ParkingSpace , Parking y Vehicle con sus respectivas propiedades

---
+++ src/Vehicle.kt
@@ -0,0 +1,13 @@
+data class Vehicle(val plate : String){
+
+    //Function states that two Vehicles are equal if their plates are equal
+    override fun equals(other : Any?) : Boolean{
+        if(other is Vehicle){
+            return this.plate == other.plate
+        }
+        return super.equals(other)
+    }
+    //Function states that the hashCode(Used internally in search functions
+    //in sets and arrays) is the hashCode of the plate
+    override fun hashCode() : Int = this.plate.hashCode()
+}
