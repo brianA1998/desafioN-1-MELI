fb1f238ec9d61294645017f8780a968974e3b5bd
arandalean <leandro.aranda@mercadolibre.com>
2021-12-20 15:43:31 -0300

    Add parkedTime property in ParkingSpace

--- src/ParkingSpace.kt
+++ src/ParkingSpace.kt
@@ -1,3 +1,7 @@
 import java.util.*
 
-data class ParkingSpace(var vehicle : Vehicle, var checkInTime : Calendar, var discountCard : String?)
+data class ParkingSpace(var vehicle : Vehicle, var checkInTime : Calendar, var discountCard : String?) {
+    private val MINUTES_IN_MILISECONDS = 60000
+    val parkedTime: Long
+        get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+}
