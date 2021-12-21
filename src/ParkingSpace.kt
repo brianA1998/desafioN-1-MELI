c5e839f6ebfa176134a85c298540e4d04f37e4b2
arandalean <leandro.aranda@mercadolibre.com>
2021-12-21 10:24:40 -0300

    Creating functions of ParkingSpace

--- src/ParkingSpace.kt
+++ src/ParkingSpace.kt
@@ -4,4 +4,30 @@ data class ParkingSpace(var vehicle : Vehicle) {
     private val MINUTES_IN_MILISECONDS = 60000
     val parkedTime: Long
         get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+
+    fun checkOutVehicle(plate : String) {
+        parking.vehicles.remove(vehicle)
+        onSuccess()
+    }
+
+    fun onSuccess(amount : Int){
+
+    }
+
+    fun calculateAmount(parkedTime : Long, vehicle: Vehicle): Int{
+        var amount = 0
+        when(parkedTime) {
+            is 0..60L, is 22L -> amount = 20
+            /*in 0..60L -> {
+                when(vehicle.type) {
+                    VehicleType.CAR -> amount = 20
+
+                }
+            }
+
+            in 60..120 -> amount = 40*/
+        }
+        return amount
+    }
+
 }
